95c49c20954589bf673fb8bc719544bdd8ba5f06
practice_repo <dropmailtonishan@gmail.com>
2025-08-15 19:03:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpWnQVliUwLFZiJMa2chRw70fiaVo-cA4",
-  authDomain: "echochat-e793e.firebaseapp.com",
-  projectId: "echochat-e793e",
-  storageBucket: "echochat-e793e.firebasestorage.app",
-  messagingSenderId: "704291585059",
-  appId: "1:704291585059:web:05f126e245e6ec494a0596",
-  measurementId: "G-V9FCKZGV9Z"
+  apiKey: "AIzaSyCSqFC70Vq045DPQTrE88H6OgSTNupDYxE",
+  authDomain: "echochat-b84ec.firebaseapp.com",
+  projectId: "echochat-b84ec",
+  storageBucket: "echochat-b84ec.firebasestorage.app",
+  messagingSenderId: "105950440425",
+  appId: "1:105950440425:web:4b65f6e5a752c92780b978",
+  measurementId: "G-PECVCT7Q2R"
 };
 
 // Initialize Firebase
